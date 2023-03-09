5b34c74b6a9768a9302bdbccdf9145b9cf50d937
MaksBmt <msolov68@gmail.com>
2023-03-09 09:01:13 +0300

    добавляет работу с sass

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,18 @@ import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
+import { js } from "./gulp/tasks/js.js";
 
 //функция наблюдатель
 function watcher() {
-  gulp.watch(path.watch.files, copy)
-  gulp.watch(path.watch.html, html)
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
 }
 
-const mainTask = gulp.parallel(copy, html)
+// Основные задачи
+const mainTask = gulp.parallel(copy, html, scss);
 
 //Построение задач выполнения сценариев
 const dev = gulp.series(reset, mainTask, gulp.parallel(watcher, server));
