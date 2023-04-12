f59804b1871dd3726d22b61cb0b39721294601fd
MaksBmt <msolov68@gmail.com>
2023-04-12 16:46:54 +0300

    фиксирует тестовые изменения

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,22 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 */
+
+// console.log('testing');
+function createVal() {
+  return Math.random()
+}
+
+function fun(val = createVal()) { }
+
+// const [a, ...b, c] = [1, 2, 3, 4]
+// console.log(a, b, c);
+// var obj = {
+//   a: 1
+// }
+//   (function (obj) {
+//     obj = {
+//       a: 2
+//     }
+//   })(obj)
+// console.log(obj.a);
